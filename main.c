63b9b81eb0d5552246db5d829689a33e285b9cc9
baikesun <curious98@naver.com>
2018-09-26 16:40:25 +0900

    fourth

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-	char c;
-	int i;
+	float a;
+	float b;
 	
-	printf("enter a character : ");
-	scanf("%c", &c);
+	printf("분자를 입력하세요 : ");
+	scanf("%f", &a);
 	
-	i = c + 1;
+	printf("분모를 입력하세요 : ");
+	scanf("%f", &b);
 	
-	printf("The next character of %c (%d) is %c (%d)", c, c, i, i);
+	printf("나누기의 결과는 %.6f 입니다.\n", a/b); 
 	
 	return 0;
 }
